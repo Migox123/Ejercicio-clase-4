92d5fa1ee2d61759ca84affc070b6cec04d3abb1
Migox123 <miguelangelholguin8@gmail.com>
2026-07-21 08:19:01 -0500

    aprendizaje del nextline , nextint, nnextdependiendo de los operadores

--- src/App.java
+++ src/App.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+
         Scanner sc = new Scanner (System.in);
+
+        System.out.println("ingrese su nombre:");
+        var nombre = sc.nextLine();
+
+        System.out.println("ingrese su edad:");
+        var edad = sc.nextInt();
+
+        System.out.println("ingrese valor doube:");
+        var valorDouble = sc.nextDouble();
+
+
         
+
+
+
+        
+
+
 
     }
 }
