e4d90ff613b14f85e5353d66ecbfedd7d49fa405
Migox123 <miguelangelholguin8@gmail.com>
2026-07-21 08:46:59 -0500

    explicacion de decimales

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
+          
         Scanner sc = new Scanner (System.in);
-
+/* 
         System.out.println("ingrese su nombre:");
         var nombre = sc.nextLine();
 
@@ -13,15 +13,17 @@ public class App {
 
         System.out.println("ingrese valor doube:");
         var valorDouble = sc.nextDouble();
+        */
+
+
 
         System.out.print("Ingrese su edad: ");
-        
 int edadUsuario = sc.nextInt(); // Lee un entero
 
 System.out.print("Ingrese su salario: ");
 double salarioUsuario = sc.nextDouble(); // Lee un double
 
-sc.nextLine(); // Consume el salto de línea pendiente
+// sc.nextLine(); // Consume el salto de línea pendiente
 
 System.out.print("Ingrese su nombre completo: ");
 String nombreUsuario = sc.nextLine(); // Lee una línea completa de texto
